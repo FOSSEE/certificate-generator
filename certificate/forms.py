c60a0cb696f19e8541422170e94ee2e717a67c13
prathamesh <pratham_920@yahoo.com>
2014-12-06 18:16:22 +0530

    commented fields

--- certificate/forms.py
+++ certificate/forms.py
@@ -4,15 +4,15 @@ from certificate.models import FeedBack
 class FeedBackForm(forms.Form):
     name = forms.CharField(max_length=30)
     email = forms.EmailField()
-    phone = forms.CharField(max_length=15)
-    organisation = forms.CharField(max_length=30)
-    department = forms.CharField\
-                (max_length=64)
-    role = forms.CharField\
-        (max_length=64)
-    address = forms.CharField(max_length=256, widget=forms.Textarea(attrs={'rows':1}))
-    city = forms.CharField(max_length=30)
-    pincode_number = forms.CharField\
-                (max_length=30)
-    state = forms.CharField\
-                (max_length=128)
+    #phone = forms.CharField(max_length=15, required=False)
+    #organisation = forms.CharField(max_length=30)
+    #department = forms.CharField\
+    #            (max_length=64)
+    #role = forms.CharField\
+    #    (max_length=64)
+    #address = forms.CharField(max_length=256, widget=forms.Textarea(attrs={'rows':1}))
+    #city = forms.CharField(max_length=30)
+    #pincode_number = forms.CharField\
+    #            (max_length=30, required=False)
+    #state = forms.CharField\
+    #            (max_length=128)
