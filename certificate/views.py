afea953bc2b8cd8084240f28a7d958689bdb34a0
Arun KP <arunkp1986@gmail.com>
2018-03-26 13:04:43 +0530

    resolved PR conflict

--- certificate/views.py
+++ certificate/views.py
@@ -1321,11 +1321,10 @@ def create_osdag_workshop_certificate(certificate_path, details, qrcode, type, p
         content = Template(template_file.read())
         template_file.close()
         content_tex = content.safe_substitute(name=details['name'].title(),
-                                              serial_key=details['serial_key'],
-                                              qr_code=qrcode,
-                                              college=details['college'],
-                                              date='%s %s' % (details['start_date'], details['year']))
-        create_tex = open('{0}{1}.tex'.format(certificate_path, file_name), 'w')
+                serial_key = details['serial_key'], qr_code=qrcode, college=details['college'],
+                date='%s %s' % (details['start_date'], details['year']))
+        create_tex = open('{0}{1}.tex'.format\
+                (certificate_path, file_name), 'w')
         create_tex.write(content_tex)
         create_tex.close()
         return_value, err = _make_certificate_certificate(certificate_path,
