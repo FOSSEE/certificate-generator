5305269c95bce82387b9e8fc8813617b89530f4f
prathamesh <pratham_920@yahoo.com>
2022-06-28 00:57:22 +0530

    Mapathon 2022 Certificates

--- certificate/views.py
+++ certificate/views.py
@@ -398,6 +398,24 @@ def verification(serial, _type):
                                           ('Team Id', user.team),
                                           ('Type', '{0} Certificate'.format(ctype)),
                                           ('Duration', '7 Dec. to 22 Dec. 2020')])
+                elif purpose == 'Mapathon 2022':
+                    user = Mapathon.objects.filter(
+                            email=certificate.email, purpose='MP2')
+                    user = user[0]
+                    if user.ctype == 'champ':
+                        ctype = 'Champion'
+                    if user.ctype == 'parti':
+                        ctype = 'Participation'
+                    if user.ctype == 'recog':
+                        ctype = 'Recognition'
+                    if user.ctype == 'ment':
+                        ctype = 'Mentor'
+                    event = 'IITB-AICTE Mapathon 2022'
+                    detail = OrderedDict([('Name', name),
+                                          ('Event', event),
+                                          ('Team Id', user.team),
+                                          ('Type', '{0} Certificate'.format(ctype)),
+                                          ('Duration', '22 February to 29 March 2022')])
                 elif purpose == "FOSSEE WINTER INTERNSHIP 2019":
                     intership_detail = Wintership.objects.get(email=certificate.email)
                     user_project_title = intership_detail.topic
@@ -989,6 +1007,8 @@ def _get_detail(serial_no):
         purpose = 'Synfig Animation Hackathon 2020'
     elif serial_no[0:3] == 'MAP':
         purpose = 'Mapathon 2020'
+    elif serial_no[0:3] == 'MP2':
+        purpose = 'Mapathon 2022'
     elif serial_no[0:3] == 'EMC':
         purpose = 'eSim Marathon 2021'
     elif serial_no[0:3] == 'EM2':
@@ -6454,12 +6474,76 @@ def mapathon_certificate_download(request):
     return render_to_response('mapathon_certificate_download.html', context, ci)
 
 
+def mapathon2_certificate_download(request):
+    context= {}
+    err = ""
+    ci = RequestContext(request)
+    cur_path = os.path.dirname(os.path.realpath(__file__))
+    certificate_path = '{0}/mapathon2/'.format(cur_path)
+    if request.method == 'POST':
+        email = request.POST.get('email').strip()
+        user = Mapathon.objects.filter(email=email, purpose='MP2')
+        if not user:
+            context["notregistered"] = 1
+            return render_to_response('mapathon2_certificate_download.html',
+                                       context, context_instance=ci)
+        user = user[0]
+        _type = 'P'
+        name = user.name
+        ctype = user.ctype
+        team = user.team
+        purpose = user.purpose
+        year = '22'
+        id =  int(user.id)
+        hexa = hex(id).replace('0x','').zfill(6).upper()
+        serial_no = '{0}{1}{2}{3}'.format(purpose, year, hexa, _type)
+        serial_key = (hashlib.sha1(serial_no)).hexdigest()
+        file_name = '{0}{1}'.format(email,id)
+        file_name = file_name.replace('.', '')
+        try:
+            old_user = Certificate.objects.get(email=email, serial_no=serial_no)
+            qrcode = 'http://fossee.in/certificates/verify/{0} '.format(old_user.short_key)
+            details = {'name': name, 'serial_key': old_user.short_key}
+            certificate = create_mapathon_certificate(certificate_path, details,
+                    qrcode, _type, ctype, team, file_name)
+            if not certificate[1]:
+                old_user.counter = old_user.counter + 1
+                old_user.save()
+                return certificate[0]
+        except Certificate.DoesNotExist:
+            uniqueness = False
+            num = 5
+            while not uniqueness:
+                present = Certificate.objects.filter(short_key__startswith=serial_key[0:num])
+                if not present:
+                    short_key = serial_key[0:num]
+                    uniqueness = True
+                else:
+                    num += 1
+            qrcode = 'http://fossee.in/certificates/verify/{0} '.format(short_key)
+            details = {'name': name,  'serial_key': short_key}
+            certificate = create_mapathon_certificate(certificate_path, details,
+                    qrcode, _type, ctype, team, file_name)
+            if not certificate[1]:
+                    certi_obj = Certificate(name=name, email=email,
+                            serial_no=serial_no, counter=1,
+                            serial_key=serial_key, short_key=short_key)
+                    certi_obj.save()
+                    return certificate[0]
+        if certificate[1]:
+            _clean_certificate_certificate(certificate_path, file_name)
+            context['error'] = True
+            context['err'] = certificate[0]
+            return render_to_response('mapathon2_certificate_download.html', context, ci)
+    context['message'] = ''
+    return render_to_response('mapathon2_certificate_download.html', context, ci)
+
 def create_mapathon_certificate(certificate_path, details, qrcode, _type,
                                 ctype, team, file_name):
     error = False
     err = None
     try:
-        download_file_name = 'MAP2020certificate.pdf'
+        download_file_name = 'MAP20222certificate.pdf'
         if ctype == 'champ':
             template = 'template_ch'
         elif ctype == 'recog':
