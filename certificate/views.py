d7006ffbc93c4455ca2bbe79328c92853b461e37
Prathamesh <pratham_920@yahoo.com>
2025-12-15 13:56:29 +0530

    Add cfd workshop

--- certificate/views.py
+++ certificate/views.py
@@ -670,6 +670,14 @@ def verification(serial, _type):
                     detail = OrderedDict([('Name', name), ('From', institute),
                                           ('Event', event), ('Date', date),
                                           ])
+                elif purpose == "CFW":
+                    participant = CertificateUser.objects.get(email=certificate.email, purpose="CFW")
+                    institute = participant.institute
+                    event = "Online Industry Readiness Workshop on CFD"
+                    date = "11 - 14 November 2025"
+                    detail = OrderedDict([('Name', name), ('From', institute),
+                                          ('Event', event), ('Date', date),
+                                          ])
                 elif purpose == "WMD":
                     participant = CertificateUser.objects.get(email=certificate.email, purpose='WMD')
                     event = "Women's Day Advanced Webinar. <br> The Power of Taking Charge"
@@ -1373,6 +1381,8 @@ def _get_detail(serial_no):
         purpose = 'IOT'
     elif serial_no[0:3] == 'INS':
         purpose = 'INS'
+    elif serial_no[0:3] == 'CFW':
+        purpose = 'CFW'
     elif serial_no[0:3] == 'WMD':
         purpose = 'WMD'
     elif serial_no[0:3] == 'DAC':
@@ -10725,4 +10735,104 @@ def create_animate_2025_certificate(certificate_path, details, qrcode, _type,
             error = True
     except Exception, e:
         error = True
+    return [None, error]
+
+
+def cfd_workshop_certificate_download(request):
+    context= {}
+    err = ""
+    ci = RequestContext(request)
+    cur_path = os.path.dirname(os.path.realpath(__file__))
+    certificate_path = '{0}/cfd-workshop/'.format(cur_path)
+    if request.method == 'POST':
+        email = request.POST.get('email').strip()
+        user = CertificateUser.objects.filter(email=email, purpose='CFW')
+        if not user:
+            context["notregistered"] = 1
+            return render_to_response('cfd_workshop_certificate_download.html',
+                                       context, context_instance=ci)
+        user = user[0]
+        _type = 'P'
+        name = user.name
+        purpose = user.purpose
+        email = user.email
+        institute = user.institute
+        institute = institute.replace('&', 'and')
+        year = '25'
+        id =  int(user.id)
+        hexa = hex(id).replace('0x','').zfill(6).upper()
+        serial_no = '{0}{1}{2}{3}'.format(purpose, year, hexa, _type)
+        serial_key = (hashlib.sha1(serial_no)).hexdigest()
+        file_name = '{0}{1}'.format(email,id)
+        file_name = file_name.replace('.', '')
+        try:
+            old_user = Certificate.objects.get(email=email, serial_no=serial_no)
+            qrcode = 'http://fossee.in/certificates/verify/{0} '.format(old_user.short_key)
+            details = {'name': name, 'serial_key': old_user.short_key}
+            certificate = create_cfd_workshop_certificate(certificate_path, details,
+                                                       qrcode, _type, institute, file_name)
+            if not certificate[1]:
+                old_user.counter = old_user.counter + 1
+                old_user.save()
+                return certificate[0]
+        except Certificate.DoesNotExist:
+            uniqueness = False
+            num = 5
+            while not uniqueness:
+                present = Certificate.objects.filter(short_key__startswith=serial_key[0:num])
+                if not present:
+                    short_key = serial_key[0:num]
+                    uniqueness = True
+                else:
+                    num += 1
+            qrcode = 'http://fossee.in/certificates/verify/{0} '.format(short_key)
+            details = {'name': name,  'serial_key': short_key}
+            certificate = create_cfd_workshop_certificate(certificate_path, details,
+                                                       qrcode, _type, institute, file_name)
+            if not certificate[1]:
+                    certi_obj = Certificate(name=name, email=email,
+                            serial_no=serial_no, counter=1,
+                            serial_key=serial_key, short_key=short_key)
+                    certi_obj.save()
+                    return certificate[0]
+        if certificate[1]:
+            _clean_certificate_certificate(certificate_path, file_name)
+            context['error'] = True
+            context['err'] = certificate[0]
+            return render_to_response('cfd_workshop_certificate_download.html', context, ci)
+    context['message'] = ''
+    return render_to_response('cfd_workshop_certificate_download.html', context, ci)
+
+
+def create_cfd_workshop_certificate(certificate_path, details,
+                                qrcode, _type, institute, file_name):
+    error = False
+    err = None
+    try:
+        download_file_name = 'CFD2025certificate.pdf'
+        template = 'template'
+        template_file = open('{0}{1}'.format(certificate_path, template), 'r')
+        content = Template(template_file.read())
+        template_file.close()
+        content_tex = content.safe_substitute(name=details['name'].title(),
+                institute=institute,
+                serial_key=encoder(details['serial_key']), qr_code=qrcode)
+        create_tex = open('{0}{1}.tex'.format(certificate_path, file_name), 'w')
+        create_tex.write(content_tex)
+        create_tex.close()
+        return_value, err = _make_certificate_certificate(certificate_path,
+                _type, file_name)
+        if return_value == 0:
+            pdf = open('{0}{1}.pdf'.format(certificate_path, file_name) , 'r')
+            response = HttpResponse(content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; \
+                    filename=%s' % (download_file_name)
+            response.write(pdf.read())
+            _clean_certificate_certificate(certificate_path, file_name)
+            return [response, False]
+        else:
+            error = True
+    except Exception, e:
+        error = True
+        print(e)
     return [None, error]
